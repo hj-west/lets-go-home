1ef7ce07272942783e6c740aea71ba013a2269d2
guswl9814 <guswl9814@fixelsoft.com>
2023-03-06 17:48:24 +0900

    project init

---
+++ src/main/java/responseor/project/letsgohome/LetsGoHomeApplication.java
@@ -0,0 +1,13 @@
+package responseor.project.letsgohome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsGoHomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetsGoHomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/responseor/project/letsgohome/LetsGoHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package responseor.project.letsgohome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsGoHomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
